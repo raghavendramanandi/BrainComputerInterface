c750ff971219083bca2475305832d214bc46b162
Rmramesh <rmramesh@inblr-rmramesh.local>
2018-05-07 23:19:50 +0530

    Sample model

--- venv/src/Test1.py
+++ venv/src/Test1.py
@@ -1,4 +1,4 @@
-!pip install -q keras #if you are on Google Colab
+#pip install -q keras #if you are on Google Colab
 import keras
 import numpy as np
 
@@ -29,30 +29,38 @@ y_train[:10]
 
 # Convert 1-dimensional class arrays to 10-dimensional class matrices
 Y_train = np_utils.to_categorical(y_train, 10)
+
 Y_test = np_utils.to_categorical(y_test, 10)
+
+print("y train:")
+print(Y_train.shape)
+# print("Y_test:")
+# print(Y_test)
+
+
 
 Y_train[:10]
 
-model = Sequential()
-model.add(Convolution2D(32, 3, 3, activation='relu', input_shape=(28,28,1)))
-model.add(Convolution2D(10, 1, activation='relu'))
-model.add(Convolution2D(10, 26))
-model.add(Flatten())
-model.add(Activation('softmax'))
-
-model.summary()
-
-model.compile(loss='categorical_crossentropy',
-             optimizer='adam',
-             metrics=['accuracy'])
-
-model.fit(X_train, Y_train, batch_size=32, nb_epoch=10, verbose=1)
-
-score = model.evaluate(X_test, Y_test, verbose=0)
-
-print(score)
-
-y_pred = model.predict(X_test)
-
-print(y_pred[:9])
-print(y_test[:9])
+# model = Sequential()
+# model.add(Convolution2D(32, 3, 3, activation='relu', input_shape=(28,28,1)))
+# model.add(Convolution2D(10, 1, activation='relu'))
+# model.add(Convolution2D(10, 26))
+# model.add(Flatten())
+# model.add(Activation('softmax'))
+#
+# model.summary()
+#
+# model.compile(loss='categorical_crossentropy',
+#              optimizer='adam',
+#              metrics=['accuracy'])
+#
+# model.fit(X_train, Y_train, batch_size=32, nb_epoch=10, verbose=1)
+#
+# score = model.evaluate(X_test, Y_test, verbose=0)
+#
+# print(score)
+#
+# y_pred = model.predict(X_test)
+#
+# print(y_pred[:9])
+# print(y_test[:9])
