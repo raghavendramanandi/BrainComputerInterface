bca62ef4151213966b520bbad4afe32b422807d8
Rmramesh <rmramesh@inblr-rmramesh.local>
2018-05-07 23:19:40 +0530

    Working model

---
+++ venv/src/KerasExample.py
@@ -0,0 +1,106 @@
+# pip install keras
+# pip install tensorflow
+import keras
+import numpy as np
+import scipy.io
+
+from keras.models import Sequential
+from keras.layers import Activation, Flatten
+from keras.layers import Convolution2D
+from keras.layers.core import Dense
+from keras.utils import np_utils
+
+from keras.datasets import mnist
+
+# Load pre-shuffled MNIST data into train and test sets
+
+# (X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+file = "/Users/rmramesh/A/Data/bcci/dataset_BCIcomp1.mat"
+data = scipy.io.loadmat(file)
+
+xdata = data.get("x_train")
+xdata = xdata.transpose(2,0,1)
+
+ydate = data.get("y_train")
+ydata = ydate[:,0]
+#
+# print(xdata.shape)
+# print(ydata.shape)
+
+
+# (X_train, y_train), (X_test, y_test) = mnist.load_data()
+newdata = xdata
+newdata = np.zeros(shape=(140,1152,1152))
+np.c_[xdata, newdata]
+
+xdata = newdata
+
+
+print (xdata[:100].shape)
+print (ydata[:100].shape)
+print (xdata[100:].shape)
+print (ydate[100:].shape)
+
+
+X_train = xdata[:100]
+
+y_train = ydata[:100]
+
+X_test = xdata[100:]
+
+y_test = ydate[100:]
+
+from matplotlib import pyplot as plt
+# %matplotlib inline
+plt.imshow(X_train[0])
+
+X_train = X_train.reshape(X_train.shape[0], 1152, 1152,1)
+X_test = X_test.reshape(X_test.shape[0], 1152, 1152,1)
+
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+
+
+y_train[:10]
+
+# Convert 1-dimensional class arrays to 10-dimensional class matrices
+Y_train = np_utils.to_categorical(y_train)
+Y_test = np_utils.to_categorical(y_test)
+
+Y_train[:10]
+
+model = Sequential()
+model.add(Convolution2D(32, 3, 3, activation='relu', input_shape=(1152,1152,1)))
+model.add(Convolution2D(10, 1, activation='relu'))
+model.add(Convolution2D(3, 1150))
+model.add(Flatten())
+model.add(Activation('softmax'))
+
+# model.add(Dense(16, activation='relu', input_shape=(1152,3,1)))
+# model.add(Dense(32, activation='relu'))
+# model.add(Dense(3, activation='softmax'))
+
+model.summary()
+
+model.compile(loss='categorical_crossentropy',
+             optimizer='adam',
+             metrics=['accuracy'])
+
+
+print("X_train:")
+print(X_train.shape)
+print("Y_train:")
+print(Y_train.shape)
+
+model.fit(X_train, Y_train, batch_size=32, nb_epoch=10, verbose=1)
+
+score = model.evaluate(X_test, Y_test, verbose=0)
+print("Score:")
+print(score)
+
+y_pred = model.predict(X_test)
+print("y predict")
+print("y test")
+print(y_pred[:9])
+print(y_test[:9])
